e53f27c7bbc03af1459b12f9aae1818e36736659
Allan Vítor. <162159186+HubOpsVitor@users.noreply.github.com>
2025-05-26 02:36:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+/**
+ * @author Allan Vítor Siqueira Silva
+
+ */
+
+// Banner carrosel ------------
+const main = document.querySelector('main')
+const backgrounds = ['url(img/liallan.jpg)','url(img/liallan1.jpg)','url(img/liallan3.jpg)']
+
+let indice = 0
+
+function slider(){
+    indice ++
+    if (indice >= backgrounds.length) {
+        indice = 0
+    }
+    //troca da imagem de background no CSS
+main.style.backgroundImage = backgrounds[indice]
+main.style.transition = 'background-image 1s ease-in-out'
+}
+
+setInterval(slider, 3000)
+
+// fim do Banner carrosel --------------------------------------------------------------
+
+
+// Inserindo o ano na tag <span> no HTML
+let ano = document.getElementById('copyrightYear')
+
+// A linha abaixo obtem o ano atual do sistema
+let anoAtual = new Date().getFullYear()
+// A linha abaixo atribui o ano a tag <span>
+ano.innerHTML = anoAtual
